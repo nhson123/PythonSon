e607698a1780d9953b0eb244c8a3965b303b4beb
hnguyen <hoangson.n@gmail.com>
2025-02-16 22:31:46 +0100

    2. commit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-firmenname = "nhs"
-berufsfeld = "Pentester"
-print("Hallo: "+firmenname + " \n ich bin dabei " + berufsfeld + " zu werden")
+alter = input('Geben Sie bitte ihr alter ein:')
+print('Sie haben eingegeben ' + str(alter) + ' als Alter')
+if int(alter) <= 18:
+    preis = 5
+# print('ihr Ticket kostet: 5 Euro')
+elif int(alter) >= 65:
+    preis = 7.5
+# print('ihr ticket kostet 7,5 Euro')
+else:
+    preis = 10
+# print('ihr Ticket kostet 10 Euro')
+print('ihr ticket kostet: ' + str(preis))
+anzahl = input('wie viele Tickets möchten sie ')
+print('dann kosten ihre' + str(anzahl)+' Tickets ingesamt: ' +
+str(int(anzahl)* float(preis)))
